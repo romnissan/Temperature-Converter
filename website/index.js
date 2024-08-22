6c55a9cbcc30f8030af479282f8c2b51035407f1
Rom Nissan <116178477+romnissan@users.noreply.github.com>
2024-08-23 00:47:25 +0300

    Add files via upload

---
+++ website/index.js
@@ -0,0 +1,23 @@
+const textBox = document.getElementById("textBox");
+const toF = document.getElementById("toF");
+const toC = document.getElementById("toC");
+const result = document.getElementById("result");
+let temp;
+
+
+function convert(){
+    if(textBox.value==""){
+        result.textContent = "Enter a number first";
+    }
+    else if(toF.checked){
+        temp = textBox.value*1.8+32;
+        result.textContent = temp+"F゜";
+    }
+    else if(toC.checked){
+        temp = (textBox.value-32)/1.8;
+        result.textContent = temp+"C゜";
+    }
+    else{
+        result.textContent = "Select a Unit";
+    }
+}
